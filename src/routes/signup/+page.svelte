150cf3335ae570073da9c0b21520ecc6522711d7
slythom <YOUR_EMAIL>
2026-02-05 22:41:56 +0100

    Fix: app.css config for Flowbite-svelte Added: Form in signup page

---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { Card, Button, Label, Input, Checkbox } from "flowbite-svelte";
+</script>
+
+<Card class="p-4 sm:p-6 md:p-8">
+  <form class="flex flex-col space-y-6" action="/">
+    <h3 class="text-xl font-medium text-gray-900 dark:text-white">Sign up to our platform</h3>
+    <Label class="space-y-2">
+      <span>Your email</span>
+      <Input type="email" name="email" placeholder="name@company.com" required />
+    </Label>
+    <Label class="space-y-2">
+      <span>Your password</span>
+      <Input type="password" name="password" placeholder="•••••" required />
+    </Label>
+    <div class="flex items-start">
+      <Checkbox>Remember me</Checkbox>
+      <a href="/" class="text-primary-700 dark:text-primary-500 ms-auto text-sm hover:underline">Lost password?</a>
+    </div>
+    <Button type="submit" class="w-full">Create account</Button>
+  </form>
+</Card>
