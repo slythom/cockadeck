805c21b9f01d54341be867861e62de4aa4581463
slythom <YOUR_EMAIL>
2026-02-05 22:34:52 +0100

    Installed: Flowbite-svelte Added: Signup/Signin pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,17 @@
+<script lang="ts">
+  import { Alert } from "flowbite-svelte";
+</script>
+
+<div class="p-8">
+  <Alert color="blue">
+    <span class="font-medium">Info alert!</span>
+    Change a few things up and try submitting again.
+  </Alert>
+</div>
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 TODO: install flowbite-svelte
 TODO: create tables in DB cockadeck
+TODO: apprendre authentification et session !!
